e9721f40cb61bd7a99da8d86994693e307f43e8f
mrv <mrv@haw>
2024-07-20 00:42:07 +0200

    - added FrostlinkURP

---
+++ Assets/Scripts/UIHandler.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIHandler : MonoBehaviour
+{
+    public GameObject[] _Panels;
+
+    private bool _showUI = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ToggleUI();
+        }
+    }
+
+    private void ToggleUI()
+    {
+        _showUI = !_showUI;
+
+        for (int i = 0; i < _Panels.Length; i++) _Panels[i].gameObject.SetActive(_showUI);
+    }
+}
